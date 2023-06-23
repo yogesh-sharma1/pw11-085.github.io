b35c31bc3cfd97a6d7de2948a6f48ecccf8be5f8
pw11-085 <yogi.krsh22@gmail.com>
2023-06-23 11:23:35 +0530

    navbar changes

--- myportfolio/src/components/Navbar.jsx
+++ myportfolio/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         >
           <ul className="flex items-center gap-1 py-2 text-lg">
             {menuLinks?.map((menu, i) => (
-              <li key={i} className={menu.class + "nav-link px-6 hover:text-cyan-600"}>
+              <li key={i} className={`${menu.class} px-6 hover:text-cyan-600`}>
                 <a href={menu?.link}>{menu?.name}</a>
               </li>
             ))}
